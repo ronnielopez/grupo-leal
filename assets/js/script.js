f8f8645ac7791b13d60353dc478f3e93aa2ccb26
ronnielopez <39448096+ronnielopez@users.noreply.github.com>
2024-04-03 15:25:43 -0600

    Arreglando a Joel

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$('input').on('change', function() {
+  $('body').toggleClass('bg-cyan-700');
+});
+
+const radioButtons = document.querySelectorAll('input[type="radio"][name="slider"]');
+const videoTags = document.querySelectorAll('video');
+
+radioButtons.forEach(radioButton => {
+  radioButton.addEventListener('change', (event) => {
+    // Derive the video ID directly from the radio button's ID
+    const selectedVideoId = event.target.id.slice(4);  // Remove "item-" prefix
+    const selectedVideo = document.getElementById(selectedVideoId);
+
+    // Apply "autoplay muted loop" attributes to the selected video
+    selectedVideo.setAttribute('autoplay', '');
+    selectedVideo.setAttribute('muted', '');
+    selectedVideo.setAttribute('loop', '');
+
+    // Remove attributes from other videos
+    videoTags.forEach(videoTag => {
+      if (videoTag !== selectedVideo) {
+        videoTag.removeAttribute('autoplay');
+        videoTag.removeAttribute('muted');
+        videoTag.removeAttribute('loop');
+      }
+    });
+  });
+});
