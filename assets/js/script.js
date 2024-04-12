99652e7de0033e95f47c45f139fa48e5c816c4f9
JoelNovoa <joelnovoa23@gmail.com>
2024-04-12 15:20:05 -0600

    build

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+const carousel3Dswiper = new Swiper(".carousel-3D-swiper", {
+  loop: true,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 3,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true
+  },
+   navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
+
 $('input').on('change', function() {
   $('body').toggleClass('');
 });
